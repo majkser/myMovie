2816a021afbda22ee2c4bddd5c74e289b8702507
mikolaj.kowalski <mikser.kowalski@gmail.com>
2024-11-08 02:13:16 +0100

    feat: added simple carusel (react-slick)

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,7 @@
 import { useState, useEffect } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function Main() {
   const [movies, setMovies] = useState([]);
@@ -6,28 +9,62 @@ export default function Main() {
   const api_key = import.meta.env.VITE_API_KEY;
 
   useEffect(() => {
-    fetch(`http://www.omdbapi.com/?apikey=${api_key}&s=${"hellboy"}`)
+    fetch(`http://www.omdbapi.com/?apikey=${api_key}&s=${"Pirates"}`)
       .then((res) => res.json())
       .then((data) => {
         setMovies(data.Search);
       });
   }, []);
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+    centerMode: true,
+    lazyLoad: "ondemand",
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <>
       <main>
-        <div className="">
+        <Slider className="w-[85%] mx-auto my-12" {...settings}>
           {movies.length > 0 &&
             movies.map((movie) => (
-              <div key={movie.imdbID} className="flex">
-                <img src={movie.Poster} alt="movie poster" />
-                <div>
-                  <h2>{movie.Title}</h2>
-                  <p>{movie.Year}</p>
-                </div>
+              <div key={movie.imdbID} className="">
+                <img
+                  className="w-64 h-96 mx-auto"
+                  src={movie.Poster}
+                  alt="movie poster"
+                />
+                <h2 className="font-bold text-center">{movie.Title}</h2>
+                <p className="text-center">{movie.Year}</p>
               </div>
             ))}
-        </div>
+        </Slider>
       </main>
     </>
   );
