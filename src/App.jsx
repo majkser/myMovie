63a669012b111c40dd4f39f7648536504e64137d
mikolaj.kowalski <mikser.kowalski@gmail.com>
2024-12-03 14:33:52 +0100

    fix: backToBroweserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ContextProvider from "./Context.jsx";
 import Header from "./components/header.jsx";
 import NewMain from "./components/NewMain.jsx";
