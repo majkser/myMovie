6dc471f238e137ca030922956696274e30e8f49a
mikolaj.kowalski <mikser.kowalski@gmail.com>
2024-12-03 14:13:33 +0100

    fix: changed http for https

--- src/https.js
+++ src/https.js
@@ -1,7 +1,7 @@
 const api_key = import.meta.env.VITE_API_KEY;
 
 export async function fetchFilmData(query) {
-  const res = await fetch(`http://www.omdbapi.com/?apikey=${api_key}${query}`);
+  const res = await fetch(`https://www.omdbapi.com/?apikey=${api_key}${query}`);
   const data = await res.json();
 
   if (!res.ok || data.Response === "False") {
